97e28bc07bcda4c57d1adc3ff41ba6d913f1987f
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-05-22 11:15:11 -0300

    funcao para calcular o imc, verificar a situacao e retornar em string

--- exercicio-1/main.js
+++ exercicio-1/main.js
@@ -6,3 +6,27 @@ function IMC(peso, altura) {
     } else reject('Valor informado precisa ser um numero.');
   });
 }
+
+function calcularIMC(peso, altura) {
+  const calculoImc = peso / (altura * altura);
+  let situacao = '';
+  switch (true) {
+    case calculoImc < 18.5:
+      situacao = 'magreza';
+      break;
+    case calculoImc >= 18.5 && calculoImc <= 24.9:
+      situacao = 'normal';
+      break;
+    case calculoImc >= 25 && calculoImc <= 29.9:
+      situacao = 'sobrepeso';
+      break;
+    case calculoImc >= 30 && calculoImc <= 39.9:
+      situacao = 'obesidade';
+      break;
+    case calculoImc >= 40:
+      situacao = 'obesidade grave';
+      break;
+  }
+
+  return `IMC: ${calculoImc.toFixed(2)} | Situação: ${situacao}`;
+}
